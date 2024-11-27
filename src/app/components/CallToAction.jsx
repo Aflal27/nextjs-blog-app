72365e81feff37a6cb102bff1a28eb2563f051fb
Aflal <mohamedaflal6719@gmail.com>
2024-11-27 16:45:40 +0530

    update project

--- src/app/components/CallToAction.jsx
+++ src/app/components/CallToAction.jsx
@@ -5,19 +5,21 @@ export default function CallToAction() {
   return (
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
       <div className="flex-1 justify-center flex flex-col">
-        <h2 className="text-2xl">Want to learn more about JavaScript?</h2>
+        <h2 className="text-2xl">Want to learn more about Web development?</h2>
         <p className="text-gray-500 my-2">
-          Checkout these resources with 100 JavaScript Projects
+          Checkout these resources with 100 Projects
         </p>
         <Button
           gradientDuoTone="purpleToPink"
           className="rounded-tl-xl rounded-bl-none">
-          <p>100 JavaScript Projects</p>
+          <a href="https://github.com/Aflal27">
+            <p>100 Projects</p>
+          </a>
         </Button>
       </div>
       <div className="p-7 flex-1">
         <img
-          src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg"
+          src="https://blog.byldgroup.com/wp-content/uploads/2020/01/Self-Development.jpg"
           alt="JavaScript usage"
           width={500}
           height={300}
